223d52bf9070f430ed9de667a66408a11481479b
leventkuru <rleventkuru@gmail.com>
2022-01-16 23:44:54 +0300

    Color Definer added

--- src/line_follower.py
+++ src/line_follower.py
@@ -28,14 +28,65 @@ class Follower:
 
                 self.twist = Twist()
 
+        def color_definer(self, image_hsv):
+
+
+            blue_lower = numpy.array([110, 0, 0])
+            blue_upper = numpy.array([130, 255, 255])
+            blue_mask = cv2.inRange(image_hsv, blue_lower, blue_upper)
+            blue_count = numpy.count_nonzero(blue_mask)
+            if blue_count > 0:
+                included = "blue"
+                return included, blue_mask
+
+            green_lower = numpy.array([36,0,0])
+            green_upper = numpy.array([86,255,255])
+            green_mask = cv2.inRange(image_hsv, green_lower, green_upper)
+            green_count = numpy.count_nonzero(green_mask)
+            if green_count > 0:
+                included = "green"
+                return included, green_mask
+
+            # lower boundary RED color range values; Hue (0 - 10)
+            lower1 = numpy.array([0, 100, 20])
+            upper1 = numpy.array([10, 255, 255])
+ 
+            # upper boundary RED color range values; Hue (160 - 180)
+            lower2 = numpy.array([160,100,20])
+            upper2 = numpy.array([179,255,255])
+ 
+            lower_mask = cv2.inRange(image_hsv, lower1, upper1)
+            upper_mask = cv2.inRange(image_hsv, lower2, upper2)
+ 
+            red_full_mask = lower_mask + upper_mask
+            red_count = numpy.count_nonzero(red_full_mask)
+            if red_count > 0:
+                included = "red"
+                return included, red_full_mask
+
+
+            yellow_lower = numpy.array([20, 10, 10])
+            yellow_upper = numpy.array([30, 255, 250])
+            yellow_mask = cv2.inRange(image_hsv, yellow_lower, yellow_upper)
+            yellow_count = numpy.count_nonzero(yellow_mask)
+            if yellow_count > 0:
+                included = "yellow"
+                return included, yellow_mask
+
+            lower_yellow = numpy.array([ 10, 10, 10])
+            upper_yellow = numpy.array([255, 255, 250])
+            nc_mask = cv2.inRange(image_hsv, lower_yellow, upper_yellow)
+            return "No color", nc_mask
+
+            
+
         def image_callback(self, msg):
             
 
                 image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
                 hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-                lower_yellow = numpy.array([ 10, 10, 10])
-                upper_yellow = numpy.array([255, 255, 250])
-                mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+                color, mask = self.color_definer(hsv)
+                print(color)
                 h, w, d = image.shape
                 search_top = 3*h/4
                 search_bot = 3*h/4 + 20
@@ -43,16 +94,18 @@ class Follower:
                 mask[int(search_bot):h, 0:w] = 0
 
                 M = cv2.moments(mask)
-                if M['m00'] > 0:
+                if color is not "No color":
+                    if M['m00'] > 0:
                         cx = int(M['m10']/M['m00'])
                         cy = int(M['m01']/M['m00'])
                         cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
-#The proportional controller is implemented in the following four lines which
-#is reposible of linear scaling of an error to drive the control output.
                         err = cx - w/2
-                        self.twist.linear.x = 0.2
+                        self.twist.linear.x = 0.3
                         self.twist.angular.z = -float(err) / 100
                         self.cmd_vel_pub.publish(self.twist)
+                else:
+                    self.twist.linear.x = 0.0
+                    self.cmd_vel_pub.publish(self.twist)
                 cv2.imshow("window", image)
                 cv2.waitKey(3)
 
